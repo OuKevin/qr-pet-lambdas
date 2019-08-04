4b5bef9c04057d981a32afa31a5211073c28594f
Kevin Ou <therealkevinou@yahoo.com>
2019-08-04 00:50:32 -0500

    testing lambda upload

---
+++ publish.js
@@ -0,0 +1,54 @@
+require('dotenv').config({ path: '../../.env' })
+
+// Load the AWS SDK for Node.js
+var AWS = require('aws-sdk');
+const path = require('path');
+const fs = require('fs');
+
+// Load credentials and set region from JSON file
+const {
+  AWS_ACCESS_KEY,
+  AWS_SECRET_KEY,
+  AWS_REGION,
+} = process.env;
+
+// Create the IAM service object
+const lambda = new AWS.Lambda({
+  apiVersion: '2015-03-31',
+  accessKeyId: AWS_ACCESS_KEY,
+  secretAccessKey: AWS_SECRET_KEY,
+  region: AWS_REGION,
+});
+
+const zipPath = `${path.resolve()}/dist/archive.zip`;
+
+console.log('zipPath', path)
+
+// var params = {
+//   Code: {
+//     ZipFile: fs.readFileSync(zipPath)
+//   },
+//   Description: "testing",
+//   FunctionName: "MyFunction",
+//   Handler: "index.handler", // is of the form of the name of your source file and then name of your function handler
+//   MemorySize: 128,
+//   Publish: true,
+//   Role: "arn:aws:iam::764074376504:role/lambda-full-access", // replace with the actual arn of the execution role you created
+//   Runtime: "nodejs10.x",
+//   Timeout: 15,
+//  };
+
+const params = {
+  FunctionName: 'MyFunction', /* required */
+  ZipFile: fs.readFileSync(zipPath),
+  Publish: false,
+}
+
+// const promise = lambda.createFunction(params).promise()
+const promise = lambda.updateFunctionCode(params).promise()
+
+promise.then((res) => {
+  console.log(res)
+}).catch(err => console.log(err))
+
+
