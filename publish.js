d6b75910b0553ffa0031ea6b024d4b050eeecad9
Kevin Ou <therealkevinou@yahoo.com>
2019-08-06 22:28:35 -0500

    seeing if the lambda is there before making an update request

--- publish.js
+++ publish.js
@@ -1,59 +1,83 @@
 require('dotenv').config({ path: '../../.env' })
 
+console.log('asdbhjasbdhjasbdjahsbdjhsa')
 // Load the AWS SDK for Node.js
 var AWS = require('aws-sdk');
 const path = require('path');
 const fs = require('fs');
-const packageJSONPath = `${path.resolve()}/package.json`;
-const packageJSON = require(packageJSONPath);
 
-const lambdaName = packageJSON.name
+const asd = async () => {
+  const packageJSONPath = `${path.resolve()}/package.json`;
+  const packageJSON = require(packageJSONPath);
+  const lambdaName = packageJSON.name
 
+  // Load credentials and set region from JSON file
+  const {
+    AWS_ACCESS_KEY,
+    AWS_SECRET_KEY,
+    AWS_REGION,
+  } = process.env;
 
-// Load credentials and set region from JSON file
-const {
-  AWS_ACCESS_KEY,
-  AWS_SECRET_KEY,
-  AWS_REGION,
-} = process.env;
+  // Create the IAM service object
+  const lambda = new AWS.Lambda({
+    apiVersion: '2015-03-31',
+    accessKeyId: AWS_ACCESS_KEY,
+    secretAccessKey: AWS_SECRET_KEY,
+    region: AWS_REGION,
+  });
 
-// Create the IAM service object
-const lambda = new AWS.Lambda({
-  apiVersion: '2015-03-31',
-  accessKeyId: AWS_ACCESS_KEY,
-  secretAccessKey: AWS_SECRET_KEY,
-  region: AWS_REGION,
-});
+  const zipPath = `${path.resolve()}/dist/archive.zip`;
 
-const zipPath = `${path.resolve()}/dist/archive.zip`;
+  try {
+    const asd = await lambda.getFunction({
+      FunctionName: lambdaName,
+    }).promise();
 
-console.log('zipPath', path)
+    const asd2 = await lambda.updateFunctionCode({
+      FunctionName: lambdaName,
+      ZipFile: fs.readFileSync(zipPath),
+      Publish: false,
+    }).promise()
+    console.log(asd2)
+  } catch(error) {
+    if (error.code === 'ResourceNotFoundException') {
+    const createFunctionParams = {
+      Code: {
+        ZipFile: fs.readFileSync(zipPath)
+      },
+      FunctionName: lambdaName,
+      Handler: "index.handler", // is of the form of the name of your source file and then name of your function handler
+      MemorySize: 128,
+      Publish: true,
+      Role: "arn:aws:iam::764074376504:role/lambda-full-access", // replace with the actual arn of the execution role you created
+      Runtime: "nodejs10.x",
+      Timeout: 15,
+    };
+    }
+      // // const promise = lambda.createFunction(params).promise()
 
-// var params = {
-//   Code: {
-//     ZipFile: fs.readFileSync(zipPath)
-//   },
-//   Description: "testing",
-//   FunctionName: lambdaName,
-//   Handler: "index.handler", // is of the form of the name of your source file and then name of your function handler
-//   MemorySize: 128,
-//   Publish: true,
-//   Role: "arn:aws:iam::764074376504:role/lambda-full-access", // replace with the actual arn of the execution role you created
-//   Runtime: "nodejs10.x",
-//   Timeout: 15,
-//  };
+    console.log({ error})
+  }
 
-const params = {
-  FunctionName: lambdaName, /* required */
-  ZipFile: fs.readFileSync(zipPath),
-  Publish: false,
+  // var params = {
+  //   Code: {
+  //     ZipFile: fs.readFileSync(zipPath)
+  //   },
+  //   FunctionName: lambdaName,
+  //   Handler: "index.handler", // is of the form of the name of your source file and then name of your function handler
+  //   MemorySize: 128,
+  //   Publish: true,
+  //   Role: "arn:aws:iam::764074376504:role/lambda-full-access", // replace with the actual arn of the execution role you created
+  //   Runtime: "nodejs10.x",
+  //   Timeout: 15,
+  //  };
+
+  // promise.then((res) => {
+  //   console.log(res)
+  // }).catch(err => console.log(err))
 }
 
-// const promise = lambda.createFunction(params).promise()
-const promise = lambda.updateFunctionCode(params).promise()
+asd()
 
-promise.then((res) => {
-  console.log(res)
-}).catch(err => console.log(err))
 
 
