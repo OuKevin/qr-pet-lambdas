55225ec27ab9038ef551f4a2a54ea7fb80c4d1b6
Kevin Ou <therealkevinou@yahoo.com>
2019-08-10 22:29:49 -0500

    added template lamba

--- packages/template-lambda/src/index.js
+++ packages/template-lambda/src/index.js
@@ -1,7 +1,7 @@
 export default async () => {
   const response = {
     statusCode: 200,
-    body: JSON.stringify('Hello from Lambda asd!!!!'),
+    body: JSON.stringify('Hello from Lambda!!!!'),
   };
   return response;
 };
