dcb977c2822e378ab13433c176b490319de7d913
Kevin Ou <therealkevinou@yahoo.com>
2019-08-10 22:15:03 -0500

    removing 2nd package

--- packages/asd1/src/index.js
+++ packages/asd1/src/index.js
@@ -1,7 +1,7 @@
 export const handler = async (event) => {
   const response = {
     statusCode: 200,
-    body: JSON.stringify('Hello from Lambda 1!!!!')
+    body: JSON.stringify('Hello from Lambda asd!!!!')
   }
   return response
 }
